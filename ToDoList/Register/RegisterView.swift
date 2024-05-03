b77b310e6dce08446b4d9f2d5320379f273766cd
Amjad Khan <amjad@backbase.com>
2024-05-03 13:07:05 +0530

    desing register view

--- ToDoList/Register/RegisterView.swift
+++ ToDoList/Register/RegisterView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State var name: String = ""
+    @State var email: String = ""
+    @State var password: String = ""
+
     var body: some View {
-        Text("Register View")
+        VStack {
+            HeaderView(title: "Register",
+                       subtitle: "Start organizing todos",
+                       rotationDegree: -15, 
+                       backgroundColor: .orange)
+
+            Spacer()
+
+            Form {
+                TextField("Name", text: $name)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .listRowSeparator(.hidden)
+                    .textInputAutocapitalization(.words)
+                    .autocorrectionDisabled()
+
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.emailAddress)
+                    .listRowSeparator(.hidden)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                    .autocorrectionDisabled()
+
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .listRowSeparator(.hidden)
+
+                TLButton(title: "Create Account",
+                         background: .blue) {
+                    createAccount()
+                }
+            }
+            .offset(y: -50)
+        }
+    }
+
+    func createAccount() {
+
     }
 }
 
